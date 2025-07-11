bdeef37d9819260296f0e48a197ed1a94b4ae759
hashaam13 <hashaamalam13@yahoo.com>
2025-07-11 16:18:04 +0300

    cifar10 data download

--- simple_flow.py
+++ simple_flow.py
@@ -1,2 +1,8 @@
 import torch
+import time
+import flow_matching.path
+import flow_matching
+import torchvision
 print(3)
+cifar=torchvision.datasets.CIFAR10(root="/home/hmuhammad/flow/data",train=True,download=True)
+
