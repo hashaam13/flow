21c87129a794d150c2de9dde6687b8f30628bd96
hashaam13 <hashaamalam13@yahoo.com>
2025-07-11 19:30:44 +0300

    data prepared for training

--- simple_flow.py
+++ simple_flow.py
@@ -7,6 +7,10 @@ from flow_matching.utils import ModelWrapper
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
 
 
 if torch.cuda.is_available():
@@ -18,6 +22,47 @@ else:
 
 torch.manual_seed(42)
 
-cifar=torchvision.datasets.CIFAR10(root="/home/hmuhammad/flowgit /data",train=True,download=True)
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
+# Training data
+trainset = torchvision.datasets.CIFAR10(
+    root='/home/hmuhammad/flow/data',
+    train=True,
+    download=True,
+    transform=transform_train
+)
+
+# Test data
+testset = torchvision.datasets.CIFAR10(
+    root='/home/hmuhammad/flow/data',
+    train=False,
+    download=True,
+    transform=transform_test
+)
+
+batch_size = 128  # You can adjust this based on your GPU memory
+
+trainloader = DataLoader(
+    trainset,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=2  # For parallel data loading
+)
+
+testloader = DataLoader(
+    testset,
+    batch_size=batch_size,
+    shuffle=False,
+    num_workers=2
+)
+print(len(testset))
