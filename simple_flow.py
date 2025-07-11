22094a1c4aa05192e31ca51e20684f24c8aabcf7
hashaam13 <hashaamalam13@yahoo.com>
2025-07-11 16:40:27 +0300

    some imports + checking gpu

--- simple_flow.py
+++ simple_flow.py
@@ -1,8 +1,23 @@
 import torch
 import time
-import flow_matching.path
-import flow_matching
+from flow_matching.path import AffineProbPath
+from flow_matching.path.scheduler import CondOTScheduler
+from flow_matching.solver import Solver, ODESolver
+from flow_matching.utils import ModelWrapper
+import matplotlib.pyplot as plt
+from matplotlib import cm
 import torchvision
-print(3)
-cifar=torchvision.datasets.CIFAR10(root="/home/hmuhammad/flow/data",train=True,download=True)
+
+
+if torch.cuda.is_available():
+    device='cuda:0'
+    print("Using gpu")
+else:
+    device='cpu'
+    print("using cpu")
+
+torch.manual_seed(42)
+
+cifar=torchvision.datasets.CIFAR10(root="/home/hmuhammad/flowgit /data",train=True,download=True)
+
 
